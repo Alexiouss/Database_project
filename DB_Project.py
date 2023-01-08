5d73d214f0bc284358c5fba5aa9cc04c2f27b92c
Alexiouss <121615113+Alexiouss@users.noreply.github.com>
2023-01-08 15:01:55 +0200

    Add files via upload

--- DB_Project.py
+++ DB_Project.py
@@ -1,6 +1,5 @@
 import sqlite3
 from sqlite3 import Error
-import Job_Finder_project as jf
 
 NumofTables=14
 
@@ -124,7 +123,7 @@ def tables():
 
     Tables[9]="""CREATE TABLE IF NOT EXISTS AGGELIA_ERGASIAS(
                                 ID_aggelias INTEGER NOT NULL,
-                                ID_paroxou INTEGER,
+                                ID_paroxou INTEGER NOT NULL,
                                 ID_kathgorias_ergasias INTEGER NOT NULL,
                                 Titlos VARCHAR(100),
                                 Topothesia VARCHAR(50) NOT NULL,
@@ -169,7 +168,7 @@ def tables():
     Tables[12]="""CREATE TABLE IF NOT EXISTS AITHSH(
                                     ID_aitoumenou INTEGER,
                                     ID_aithshs INTEGER NOT NULL,
-                                    ID_aggelias INTEGER NOT NULL,
+                                    ID_aggelias INTEGER,
                                     Hmeromhnia_aithshs DATE NOT NULL,
                                     PRIMARY KEY(ID_aithshs),
                                     FOREIGN KEY(ID_aitoumenou) REFERENCES PROFIL_AITOUMENOY(ID_aitoumenou)
@@ -595,7 +594,7 @@ def in_ikanotita(conn):
     cur.close()
     return None
 
-def main():
+def Create_db():
     database="DB_project.db"
     sql_create_tables=tables()
     conn=create_connection(database)
@@ -617,64 +616,5 @@ def main():
     except:
         print("Data already inserted")
     conn.commit()
-    print("-----Welcome to Job_Finder-----\nPlease create an account if you dont have one\nAlready have an account?\nPress Sign in to continue\n")
-    print("Press 1 for Sign up or 2 for Sign in")
-    signing=int(input())
-    if(signing==1):
-        data=jf.SignUP().Sign_UP()
-        print("\nΑς ξεκινήσουμε με την δημιουργία του προφίλ σας.\n")
-        id_assignement=jf.User_id_assignement(data["Email"])
-        if data["Eidos_xrhsth"]=='A':
-           jf.Profile_Creation().Create_ait_profil(data["Email"])
+    return None
 
-           jf.Empeiria().Create_education(id_assignement)
-           
-           jf.Empeiria().Create_proyphresia(id_assignement)
-
-           jf.Empeiria().Create_Ikanothta_ypopsifiou(id_assignement)
-
-        elif data["Eidos_xrhsth"]=='P':
-            jf.Profile_Creation().Create_paroxos_profil(data["Email"])
-            jf.Aggelia().Create_aggelia_erg(id_assignement)
-    
-    elif(signing==2):
-        user=jf.SignIn().Sign_In()
-        if(user[1]=='A'):
-            while True:
-                x=int(input("Διαλέξτε μία λειτουργία:\n0:Sign out\n1:Αναζήτηση και αίτηση αγγελιών\n2:Αξιολόγηση παρόχου\n"))
-                if(x==0):
-                    break
-                while True:
-                    if(x==1):
-                        jf.Anazhthsh_aggelion().Search_aggelies()
-                        n=int(input("\nΑν θέλετε να κάνετε αίτηση σε κάποια αγγελία πατήστε 1 αλλιώς πατήστε 0:"))
-                        if(n==1):
-                            jf.Aithsh().Create_Aithsh(user[0])
-                            break
-                        else:
-                            break
-                    elif(x==2):
-                        jf.Aksiologhsh().Create_aksiologhsh(user[0])
-                        break
-        elif(user[1]=='P'):
-            while True:
-                x=int(input("Διαλέξτε μία λειτουργία:\n0:Sign out\n1:Αναζήτηση των αιτήσεων σας\n2:Εμφάνιση αξιολόγησης\n3:Δημιουργία αγγελίας\n"))
-                if(x==0):
-                    break
-                while True:
-                    if(x==1):
-                        jf.Show_aitisis().show_aggelies(user[0])
-                        break
-                    elif(x==2):
-                        jf.Show_aitisis().show_aksiologhsh(user[0])
-                        break
-                    elif(x==3):
-                        jf.Aggelia().Create_aggelia_erg(user[0])
-                        break
-    else:
-        jf.Delete()
-    conn.commit()
-    conn.close()
-
-if __name__=="__main__":
-    main()
